bac62535af75a92ce06fcea4ac59a42ba0f9a52a
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-12 08:40:35 +0700

    cakra

---
+++ src/main/java/com/project/accounts/model/account_nasabah.java
@@ -0,0 +1,26 @@
+package com.project.accounts.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class account_nasabah {
+	
+	@Id
+	@Column (name = "nama_nasabah")
+	private String nama_nasabah;
+	@Column (name = "email_nasabah")
+	private String email_nasabah;
+	@Column (name = "no_rek")
+	private int no_rek;
+	@Column (name = "balance")
+	private double balance;
+	@Column (name = "status_rek")
+	private String status_rek;
+
+}
