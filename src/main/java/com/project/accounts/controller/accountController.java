f0a2a2d532510c1b23b4669512e3b7a3e15c911a
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-12 15:58:07 +0700

    cakra

---
+++ src/main/java/com/project/accounts/controller/accountController.java
@@ -0,0 +1,13 @@
+package com.project.accounts.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+public class accountController {
+	
+	//inquiry no rekening
+	
+	//update balance
+}
