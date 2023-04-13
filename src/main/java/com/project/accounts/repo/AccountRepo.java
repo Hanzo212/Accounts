7e358bcf345251bd6d456fbbed05eeea28ff5b58
L-ACER\Cakra <hanzohattori404@gmail.com>
2023-04-13 09:02:34 +0700

    cakra

--- src/main/java/com/project/accounts/repo/AccountRepo.java
+++ src/main/java/com/project/accounts/repo/AccountRepo.java
@@ -1,5 +1,7 @@
 package com.project.accounts.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +16,7 @@ public interface AccountRepo extends JpaRepository<AccountNasabah, String>{
 	@Transactional
 	@Query(value="Update account_nasabah set balance=?1 where no_rek=?2")
 	void updateBalance(Double balance, int no_rek);
+	
+	@Query(value="select balance, status_rek, type_rek from account_nasabah where no_rek=?1")
+	List<AccountNasabah> x (int no_rek);
 }
--- src/main/java/com/project/accounts/service/AccountService.java
+++ src/main/java/com/project/accounts/service/AccountService.java
@@ -1,8 +1,12 @@
 package com.project.accounts.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.project.accounts.model.AccountNasabah;
 import com.project.accounts.repo.AccountRepo;
 
 @Service
@@ -10,5 +14,9 @@ public class AccountService {
 	
 	@Autowired
 	AccountRepo accountRepo;
+	
+	public List<AccountNasabah> getAccountByNoRek(int no_rek) {
+		return accountRepo.x(no_rek);
+	}
 
 }
